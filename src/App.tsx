3c22a294d35bd2f4435cbc0060bb1d67c5a1c90d
Lenin Ceballos <leninceballos@gmail.com>
2025-06-04 15:02:12 -0500

    Add Vite React setup with index.html

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// /src/App.tsx
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="p-6 text-xl font-semibold">
+      MongoDB Log Insight App is running! 🎉
+    </div>
+  )
+}
+
+export default App
