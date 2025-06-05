a4330ac949e4e986c17ba232cbff16028b3a0273
Lenin Ceballos <leninceballos@gmail.com>
2025-06-04 21:14:45 -0500

    Add index.html and fix vite.config.js for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   root: '.',
@@ -10,4 +6,3 @@ export default defineConfig({
     emptyOutDir: true
   }
 })
-
