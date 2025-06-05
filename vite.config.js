829299808bb329ae83c8b14d3e903284962bef2f
Lenin Ceballos <leninceballos@gmail.com>
2025-06-04 21:23:44 -0500

    Update project files excluding .env.local

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // 👈 Explicitly define root where index.html is
   plugins: [react()],
   build: {
     outDir: 'dist',
